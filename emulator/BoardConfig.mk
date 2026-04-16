843811427c44fe3e30169c4d24bfb1df56d10f20
Gabriel Borges <gabriel.borges@cariad.us>
2026-04-16 03:17:53 +0000

    Create custom emulator device for tonal challenge

---
+++ emulator/BoardConfig.mk
@@ -0,0 +1,4 @@
+include device/generic/goldfish/board/emu64x/BoardConfig.mk
+
+# TODO: ADD SEPOLICY DIR
+# BOARD_VENDOR_SEPOLICY_DIRS += device/challenge/proximity_plus/sepolicy
---
+++ emulator/emulator.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/generic/goldfish/64bitonly/product/sdk_phone64_x86_64.mk)
+
+# Product properties
+PRODUCT_NAME := emulator
+PRODUCT_DEVICE := emu64x
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Android SDK x86_64 emulator for Tonal Challenge
